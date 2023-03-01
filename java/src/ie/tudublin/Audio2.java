c5cbc4f6ef62b4726b435e07e5fcf736ff54aba2
iLeanna0 <itoufekoula@gmail.com>
2023-03-01 17:01:06 +0000

    audio2

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,6 +4,7 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class Audio2 extends PApplet
@@ -13,9 +14,11 @@ public class Audio2 extends PApplet
     AudioInput ai;
     AudioBuffer ab;
 
+    FFT fft;
+
     public void settings()
     {
-        size(1024, 1000);
+        size(1024, 1024);
     }
 
     public void setup()
@@ -25,6 +28,8 @@ public class Audio2 extends PApplet
         ab = ai.mix;
         lerpedBuffer = new float[width];
 
+        fft = new FFT(width, 44100);
+
     }
 
     float[] lerpedBuffer;
@@ -32,14 +37,35 @@ public class Audio2 extends PApplet
     {
         background(0);
         colorMode(HSB);
+        stroke(255);
         float half=height/2;
         for (int i = 0; i<ab.size(); i++)
         {
             stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
             float f= abs(lerpedBuffer[i] *half * 2.0f);
-            line(i, half + f, i, half - f);
+            line(i, half + f, i , half - f);
         }
+
+        fft.forward(ab);
+        stroke(255);
+
+        int highestIndex = 0;
+        for (int i = 0; i < fft.specSize(); i++)
+        {
+            line(i * 2.0f, height, i*2.0f, height - fft.getBand(i) * 5.0f);
+
+            if(fft.getBand(i) > fft.getBand(highestIndex))
+            {
+                highestIndex=i;
+            }
+        }
+
+        float freq = fft.indexToFreq(highestIndex);
+        fill(255);
+        textSize(20);
+        text("Freq: ", +freq, 100, 200);
+
         //println(map(5, 0, 10, 1000, 2000));
         //println(map2(a:5, b:2, c:10, d:1000, e:2000));
 
