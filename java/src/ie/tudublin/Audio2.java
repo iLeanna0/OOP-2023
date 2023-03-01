a9253a434d563b88d900ff65822cf4ea5a1477d1
iLeanna0 <itoufekoula@gmail.com>
2023-03-01 18:06:50 +0000

    PitchSpeller

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,27 +4,31 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
-public class Audio2 extends PApplet
-{
+public class Audio2 extends PApplet{
+
     Minim m;
-    AudioPlayer ap;
     AudioInput ai;
+    AudioPlayer ap;
     AudioBuffer ab;
+
+    FFT fft;
 
     public void settings()
     {
-        size(1024, 1000);
+        size(1024, 1024);
     }
 
     public void setup()
     {
         m = new Minim(this);
-        ai= m.getLineIn(Minim.MONO, width, 44100, 16);
+        ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix;
         lerpedBuffer = new float[width];
 
+        fft = new FFT(width, 44100);
     }
 
     float[] lerpedBuffer;
@@ -32,31 +36,55 @@ public class Audio2 extends PApplet
     {
         background(0);
         colorMode(HSB);
-        float half=height/2;
-        for (int i = 0; i<ab.size(); i++)
+        stroke(255);
+        float half = height / 2;
+        for(int i = 0 ; i < ab.size() ; i ++)
         {
             stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-            float f= abs(lerpedBuffer[i] *half * 2.0f);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+            float f = abs(lerpedBuffer[i] * half * 2.0f);
             line(i, half + f, i, half - f);
         }
-        //println(map(5, 0, 10, 1000, 2000));
-        //println(map2(a:5, b:2, c:10, d:1000, e:2000));
 
-    }  
-    
-    /*float map1(float a, float b, float c, float d, float e)
-    {
-        float f = (a - b) / (c -b ); new
-        return d + f * (e - d);
+        fft.forward(ab);
+        stroke(255);
+
+        int highestIndex = 0;
+        for(int i = 0 ;i < fft.specSize() / 2 ; i ++)
+        {
+            line(i * 2.0f, height, i * 2.0f, height - fft.getBand(i) * 5.0f);
+
+            if (fft.getBand(i) > fft.getBand(highestIndex))
+            {
+                highestIndex = i;
+            }
+        }
+
+        float freq = fft.indexToFreq(highestIndex);
+        fill(255);
+        textSize(20);
+        text("Freq: " + freq, 100, 100);
+
+        float y = map(freq, 1000.0f, 2500.0f, height, 0);
+        lerpedY = lerp(lerpedY, y, 0.1f);
+        circle(200, y, 50);
+        circle(300, lerpedY, 50);
+        
+
+
+
+        //println(map(5, 2, 10, 1000, 2000));
+        //println(map1(5, 2, 10, 1000, 2000));
     }
-    
-    float map2(float a, float b, float c, float d, float e)
-    {
-        float range1= c - b;
-        float range2= e - d;
-        float howFar= a - b;
 
-        return d + (howFar / range1)* range2;
-    }*/
+    float lerpedY = 0;
+    
+    float map1(float a, float b, float c, float d, float e)
+    {
+        float range1 = c - b;
+        float range2 = e - d;
+        float howFar = a - b;
+
+        return d + (howFar / range1) * range2;
+    }
 }
